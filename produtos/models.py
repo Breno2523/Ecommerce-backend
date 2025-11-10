3d8d48d8dc535a6cbf45fcd4873e8e6b7d96ffb3
Breno <marianobreno245@gmail.com>
2025-11-10 18:29:01 -0300

    imagem para url_foto

--- produtos/models.py
+++ produtos/models.py
@@ -4,7 +4,7 @@ class Produto(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.TextField()
     preco = models.DecimalField(max_digits=10, decimal_places=2)
-    imagem = models.URLField(blank=True, null=True)
+    url_foto = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return self.nome
